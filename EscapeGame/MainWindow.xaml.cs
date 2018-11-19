12d01e82e048168b31168b8ddf501bd223908822
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-19 23:08:04 +0100

    Dernière modification

--- EscapeGame/MainWindow.xaml.cs
+++ EscapeGame/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace EscapeGame
         {
             InitializeComponent();
         }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
     }
 }
